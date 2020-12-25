8bc5833edb6fd6175775dfe171ca9c4da07942b0
rkhimka <romkhimka@gmail.com>
2020-12-25 16:42:02 +0200

    created separate methods for group creation. Added GroupData class

--- src/test/java/com/testpr/addressbook/AddGroupTest.java
+++ src/test/java/com/testpr/addressbook/AddGroupTest.java
@@ -19,29 +19,50 @@ public class AddGroupTest {
         wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         //make full screen browser
         wd.manage().window().maximize();
-        //open addressbook
-        wd.get("http://localhost/addressbook/group.php");
-        //login
-        wd.findElement(By.xpath(".//input[@name=\"user\"]")).sendKeys("admin");
-        wd.findElement(By.xpath(".//input[@name=\"pass\"]")).sendKeys("secret");
+        openAddressbook();
+        login("admin", "secret");
+    }
+
+    private void login(String username, String password) {
+        wd.findElement(By.xpath(".//input[@name=\"user\"]")).sendKeys(username);
+        wd.findElement(By.xpath(".//input[@name=\"pass\"]")).sendKeys(password);
         wd.findElement(By.xpath(".//input[@value=\"Login\"]")).click();
+    }
+
+    private void openAddressbook() {
+        wd.get("http://localhost/addressbook/");
     }
 
     @Test
     public void addGroup() {
-        //click add group btn
-        wd.findElement(By.xpath(".//input[@value=\"New group\"][1]")).click();
+        followGroups();
+        initGroupCreation();
+        setGroupData(new GroupName("test group4", "test header4", "test comment4"));
+        submitGroupCreation();
+    }
+
+    private void submitGroupCreation() {
+        wd.findElement(By.xpath(".//input[@value=\"Enter information\"]")).click();
+    }
+
+    private void setGroupData(GroupName groupName1) {
         //set group name
         wd.findElement(By.xpath(".//input[@name=\"group_name\"]")).clear();
-        wd.findElement(By.xpath(".//input[@name=\"group_name\"]")).sendKeys("test group2");
+        wd.findElement(By.xpath(".//input[@name=\"group_name\"]")).sendKeys(groupName1.getGroupName());
         //add header
         wd.findElement(By.xpath(".//textarea[@name=\"group_header\"]")).clear();
-        wd.findElement(By.xpath(".//textarea[@name=\"group_header\"]")).sendKeys("test header2");
+        wd.findElement(By.xpath(".//textarea[@name=\"group_header\"]")).sendKeys(groupName1.getHeaderName());
         //add comment
         wd.findElement(By.xpath(".//textarea[@name=\"group_footer\"]")).clear();
-        wd.findElement(By.xpath(".//textarea[@name=\"group_footer\"]")).sendKeys("test comment2");
-        // submit
-        wd.findElement(By.xpath(".//input[@value=\"Enter information\"]")).click();
+        wd.findElement(By.xpath(".//textarea[@name=\"group_footer\"]")).sendKeys(groupName1.getComment());
+    }
+
+    private void initGroupCreation() {
+        wd.findElement(By.xpath(".//input[@value=\"New group\"][1]")).click();
+    }
+
+    private void followGroups() {
+        wd.findElement(By.xpath(".//a[text()='groups']")).click();
     }
 
     @AfterMethod
---
+++ src/test/java/com/testpr/addressbook/GroupName.java
@@ -0,0 +1,25 @@
+package com.testpr.addressbook;
+
+public class GroupName {
+    private final String groupName;
+    private final String headerName;
+    private final String comment;
+
+    public GroupName(String groupName, String headerName, String comment) {
+        this.groupName = groupName;
+        this.headerName = headerName;
+        this.comment = comment;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public String getHeaderName() {
+        return headerName;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+}
